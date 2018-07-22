0ff86646b7f5439619ac0a1a8c286a3154e81e13
Norman Coloma García <ua.norman@gmail.com>
2018-07-22 12:48:56 +0200

    Removed unused imports

--- src/main/java/com/films/films/batch/UploadReportTasklet.java
+++ src/main/java/com/films/films/batch/UploadReportTasklet.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Component
