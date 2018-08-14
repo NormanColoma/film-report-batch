bd22842bd49ea1486fa74a5a323d74add1b47ae9
Norman Coloma García <ua.norman@gmail.com>
2018-08-14 22:24:03 +0200

    Removed unused queue and bingins from rabbitmq config

--- src/main/java/com/films/films/batch/configuration/FilmReportProducerConfiguration.java
+++ src/main/java/com/films/films/batch/configuration/FilmReportProducerConfiguration.java
@@ -2,10 +2,7 @@ package com.films.films.batch.configuration;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
@@ -16,22 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @Data
 public class FilmReportProducerConfiguration {
     private final String directExchange = "films-report-generated";
-    private final String queueName = "film-reporter";
     private final String routingKey = "films.report.mail";
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
 
     @Bean
     DirectExchange exchange() {
         return new DirectExchange(directExchange);
-    }
-
-    @Bean
-    Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
     }
 
     @Bean
